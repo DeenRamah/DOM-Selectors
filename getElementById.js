36273d3dd26b8a31d528410cc228c42bf8f59d5d
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-27 08:32:55 +0300

    Create getElementById.js

---
+++ getElementById.js
@@ -0,0 +1,5 @@
+<h1 id="logo">Deen Ramah</h1>
+<script>
+  const logo = document.getElementById('logo');
+  const img = document.getElementById('img');// null since there is no elemnt with same id
+</script>
